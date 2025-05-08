ae0e6c7f1184af9860e7c3103a96b0d397deea7b
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-08 14:57:39 +0200

    [UPD]Refactor Accordion components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import AccordionButton from './components/AccordionButton';
+import AccordionContent from './components/AccordionContent';
 
 function App() {
   const [count, setCount] = useState(0)
 
-  return ();
+  return (
+    <>
+      <div className='big-container'>
+        <h1>Learn Web Development</h1>
+      </div>
+      <AccordionButton />
+      <AccordionContent />
+    </>
+  );
 };
 
 export default App
