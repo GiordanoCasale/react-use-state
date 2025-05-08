4fc035b4f0abe0db70bf8cf17c91c64c323dc793
GiordanoCasale <giordanocasale8@gmail.com>
2025-05-08 14:25:38 +0200

    [UPD]Create AccordionButton and AccordionContent components

---
+++ src/components/AccordionButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccordionButton = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AccordionButton
---
+++ src/components/AccordionContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AccordionContent = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AccordionContent
